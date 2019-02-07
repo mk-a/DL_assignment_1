0c76e5e1836c60efbe93312afabaa530719e20ed
adrien <adrien.mainka@gmail.com>
2019-02-07 18:53:38 -0500

    added activation functions. tanh and sigmoid not working though

--- mlp.py
+++ mlp.py
@@ -2,6 +2,7 @@
 import numpy as np
 import math
 import sys
+import time
 
 # def softmax(x, c = 1):
 #     """Compute softmax values for each sets of scores in x."""
@@ -39,12 +40,58 @@ def onehot(Y, m):
         ret[i,Y[i]]=1.
     return ret.astype(int)
 
+class Activation_function:
+    def __call__(self, x):
+        raise NotImplementedError()
+    def derivative(self, x):
+        raise NotImplementedError()
+
+class Linear(Activation_function):
+    def __call__(self, x):
+        return x
+        def derivative(self, x):
+            return 1.
+
+class Sigmoid(Activation_function):
+    def __call__(self, x):
+        return 1./(np.exp(-x)+1.)
+    def derivative(self, x):
+        return self(x)*(1. - self(x))
+
+class ReLU(Activation_function):
+    def __call__(self, x):
+        return x + (x > 0)
+    def derivative(self, x):
+        return 1. * (x > 0)
+
+class Tanh(Activation_function):
+    def __call__(self, x):
+        tmp1 = np.exp(x)
+        tmp2 = np.exp(-x)
+        return (tmp1  - tmp2) / (tmp1 + tmp2)
+    def derivative(self, x):
+        return 1. - self(x)**2
+
 
 class MLP:
-    def __init__(self, input_size, n1, n2, output_size, c=1, init="normal"):
+    """ A simple implementation of a multi layers perceptron with two hidden layers. """
+    def __init__(self, input_size, n1, n2, output_size, c=1, init="normal", activation="linear"):
         n_parameter = n1 * (input_size + 1) + n2 * (n1 + 1) + output_size * (n2 + 1)
-        print("Number of parameters : "+str(n_parameter))
+        print("Input dimension {:d}\tLayer 1 dimension {:d}\tLayer 2 dimension {:d}\tOutput dimension {:d}".format(input_size, n1, n2, output_size))
+        print("Total number of parameters : {:d}".format(n_parameter))
+
         self.output_size = output_size
+        if activation == "linear":
+            self.activation = Linear()
+        elif activation == "sigmoid":
+            self.activation = Sigmoid()
+        elif activation=="relu":
+            self.activation = ReLU()
+        elif activation=="tanh":
+            self.activation = Tanh()
+        else :
+            self.activation = activation
+
         if init == "normal":
             self.W1 = np.random.normal(size=(n1, input_size))
             self.W2 = np.random.normal(size=(n2, n1))
@@ -69,38 +116,49 @@ class MLP:
 
     def fprop(self, X):
         self.X=X
-        self.h1 = np.dot(self.W1, X.T ) + self.b1
-        self.h2 = np.dot(self.W2, self.h1 ) + self.b2
+        self.a1 = np.dot(self.W1, X.T ) + self.b1
+        self.h1 = self.activation(self.a1)
+
+        self.a2 = np.dot(self.W2, self.h1 ) + self.b2
+        self.h2 = self.activation(self.a2)
 
         self.oa = np.dot(self.W3, self.h2 ) + self.b3
-
         self.os = softmax(self.oa, self.c)
 
     def bprop(self, Y, learning_rate):
-        # L = cross_entropy(self.os, Y)
+        """ We assume fprop has been run before bprop
+            This function calculate the gradient and update the weights after
+        """
+
+        batch_size = Y.size
+
         self.grad_oa = self.os - onehot(Y,self.output_size).T
         self.grad_w3 = np.dot(self.grad_oa, self.h2.T)
         self.grad_b3 = self.grad_oa
 
         self.grad_h2 = np.dot(self.W3.T, self.grad_oa)
-        self.grad_w2 = np.dot(self.grad_h2, self.h1.T)
+        self.grad_a2 = self.grad_h2 * self.activation.derivative(self.a2)
+        self.grad_w2 = np.dot(self.grad_a2, self.h1.T)
         self.grad_b2 = self.grad_h2
 
         self.grad_h1 = np.dot(self.W2.T, self.grad_h2)
-        self.grad_w1 = np.dot(self.grad_h1, self.X)
+        self.grad_a1 = self.grad_h1 * self.activation.derivative(self.a1)
+        self.grad_w1 = np.dot(self.grad_a1, self.X)
         self.grad_b1 = self.grad_h1
-        
-        self.W3 -= learning_rate * self.grad_w3
-        self.b3 -= learning_rate * self.grad_b3.sum(axis=1).reshape((-1,1)) / self.grad_b3.shape[1]
 
-        self.W2 -= learning_rate * self.grad_w2
-        self.b2 -= learning_rate * self.grad_b2.sum(axis=1).reshape((-1,1)) / self.grad_b2.shape[1]
+        self.W3 -= learning_rate * self.grad_w3 / batch_size
+        self.b3 -= learning_rate * self.grad_b3.sum(axis=1).reshape((-1,1)) / batch_size
 
-        self.W1 -= learning_rate * self.grad_w1
-        self.b1 -= learning_rate * self.grad_b1.sum(axis=1).reshape((-1,1)) / self.grad_b2.shape[1]
+        self.W2 -= learning_rate * self.grad_w2 / batch_size
+        self.b2 -= learning_rate * self.grad_b2.sum(axis=1).reshape((-1,1)) / batch_size
+
+        self.W1 -= learning_rate * self.grad_w1 / batch_size
+        self.b1 -= learning_rate * self.grad_b1.sum(axis=1).reshape((-1,1)) / batch_size
 
     def train(self,X, Y, epochs, batch_size, learning_rate):
+        train_time = time.time()
         for epoch in range(epochs):
+            epoch_time = time.time()
             pred = 0
             for i in range(math.ceil(X.shape[0]/batch_size)):
                 i_min = i * batch_size
@@ -110,14 +168,12 @@ class MLP:
                 self.bprop(Y[i_min:i_max], learning_rate)
 
                 pred += np.sum( (np.argmax(self.os,axis=0) == Y[i_min:i_max] ).astype(int) )
+                print("Epoch {:d}/{:d}\tExamples {:d}/{:d}\tAccuracy {:.3f}\tEpoch time {:.2f}s\tTraining time {:.2f}s".format(epoch+1, epochs, i_max, X.shape[0], pred/i_max, time.time() - epoch_time, time.time() - train_time), end='\r')
+            print("Epoch {:d}/{:d}\tExamples {:d}/{:d}\tAccuracy {:.3f}\tEpoch time {:.2f}s\tTraining time {:.2f}s".format(epoch+1, epochs, i_max, X.shape[0], pred/i_max, time.time() - epoch_time, time.time() - train_time))
 
-                print("Epoch "+str(epoch+1)+"/"+str(epochs)+"\tExamples "+str(i_max)+"/"+
-                    str(X.shape[0])+"\tRealtime accuracy :"+'{:.3f}'.format(pred/i_max),end='\r')
-            # print("Epoch "+str(epoch+1)+"/"+str(epochs)+"\tFinal accuracy :"+str(self.evaluate(X,Y)))
-            print("Epoch "+str(epoch+1)+"/"+str(epochs)+"\tExamples "+str(i_max)+"/"+
-                str(X.shape[0])+"\tRealtime accuracy :"+'{:.3f}'.format(pred/i_max)+"\tFinal accuracy :"+'{:.3f}'.format(self.evaluate(X,Y)) )
             if np.isnan(self.W1).any() :
                 sys.exit("ERROR : The parameters contain NaNs. Use a smaller learning rate.")
+        print("Total training time {:.2f}s".format(time.time() - train_time))
 
     def predict(self,X):
         self.fprop(X)
