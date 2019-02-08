77160b7f1a39f60864232ea7a528cb840c119edf
adrien <adrien.mainka@gmail.com>
2019-02-08 14:07:11 -0500

    add l1 & l2 regularizers, tanh and sigmoid work

--- mlp.py
+++ mlp.py
@@ -41,30 +41,40 @@ def onehot(Y, m):
     return ret.astype(int)
 
 class Activation_function:
+    def __str__(self):
+        return "Activation function"
     def __call__(self, x):
         raise NotImplementedError()
     def derivative(self, x):
         raise NotImplementedError()
 
-class Linear(Activation_function):
+class Identity(Activation_function):
+    def __str__(self):
+        return "identity"
     def __call__(self, x):
         return x
         def derivative(self, x):
             return 1.
 
 class Sigmoid(Activation_function):
+    def __str__(self):
+        return "sigmoid"
     def __call__(self, x):
         return 1./(np.exp(-x)+1.)
     def derivative(self, x):
         return self(x)*(1. - self(x))
 
 class ReLU(Activation_function):
+    def __str__(self):
+        return "ReLU"
     def __call__(self, x):
         return x + (x > 0)
     def derivative(self, x):
         return 1. * (x > 0)
 
 class Tanh(Activation_function):
+    def __str__(self):
+        return "tanh"
     def __call__(self, x):
         tmp1 = np.exp(x)
         tmp2 = np.exp(-x)
@@ -73,16 +83,55 @@ class Tanh(Activation_function):
         return 1. - self(x)**2
 
 
-class MLP:
+class MLP_2L:
     """ A simple implementation of a multi layers perceptron with two hidden layers. """
-    def __init__(self, input_size, n1, n2, output_size, c=1, init="normal", activation="linear"):
+    def __init__(self, input_size, n1, n2, output_size, c=1, init="normal",\
+                    activation="identity", l1=0, l2=0):
+        """ Initialises a two layers MLP.
+
+            Parameters
+            ----------
+
+            input_size : int
+                Input dimension
+
+            n1 : int
+                First layer dimension
+
+            n2 : int
+                Second layer dimension
+
+            output_size : int
+                Output dimension
+
+            c : float
+                Scalar, the output layer is softmax(c * oa), with oa the preactivation.
+                By default c = 1. c must be strictly greater than 0.
+
+            init : String
+                Defines the weights initialization process. init values must be "normal",
+                "zeros" or "glorot". By default init = "normal"
+
+            activation : String
+                Defines the activation function used on the two layers. activation must be
+                "identity", "sigmoid", "relu" or tanh. By default activation="identity"
+
+            l1 : float
+                weigth of the L1 regularizer. Must be positive. By default l1 = 0
+                : no L1 regularizer
+
+            l2 : float
+                weigth of the L2 regularizer. Must be positive. By default l2 = 0
+                : no L2 regularizer
+        """
+
         n_parameter = n1 * (input_size + 1) + n2 * (n1 + 1) + output_size * (n2 + 1)
-        print("Input dimension {:d}\tLayer 1 dimension {:d}\tLayer 2 dimension {:d}\tOutput dimension {:d}".format(input_size, n1, n2, output_size))
+        print("Input dimension {:d}\tLayer 1 dimension {:d}\tLayer 2 dimension {:d}\tOutput dimension {:d}\t Initilization method {:s}\tActivation function {:s}".format(input_size, n1, n2, output_size, init, str(activation)))
         print("Total number of parameters : {:d}".format(n_parameter))
 
         self.output_size = output_size
-        if activation == "linear":
-            self.activation = Linear()
+        if activation == "identity":
+            self.activation = Identity()
         elif activation == "sigmoid":
             self.activation = Sigmoid()
         elif activation=="relu":
@@ -91,6 +140,9 @@ class MLP:
             self.activation = Tanh()
         else :
             self.activation = activation
+
+        self.l1 = l1
+        self.l2 =l2
 
         if init == "normal":
             self.W1 = np.random.normal(size=(n1, input_size))
@@ -133,18 +185,18 @@ class MLP:
         batch_size = Y.size
 
         self.grad_oa = self.os - onehot(Y,self.output_size).T
-        self.grad_w3 = np.dot(self.grad_oa, self.h2.T)
-        self.grad_b3 = self.grad_oa
+        self.grad_w3 = np.dot(self.grad_oa, self.h2.T) + self.l1 * self.W3 / ( np.abs(self.W3) + (self.W3 == 0).astype(int) ) + 2 * self.l2 * self.W3
+        self.grad_b3 = self.grad_oa + self.l1 * self.b3 / ( np.abs(self.b3) + (self.b3 == 0).astype(int) ) + 2 * self.l2 * self.b3
 
         self.grad_h2 = np.dot(self.W3.T, self.grad_oa)
         self.grad_a2 = self.grad_h2 * self.activation.derivative(self.a2)
-        self.grad_w2 = np.dot(self.grad_a2, self.h1.T)
-        self.grad_b2 = self.grad_h2
+        self.grad_w2 = np.dot(self.grad_a2, self.h1.T) + + self.l1 * self.W2 / ( np.abs(self.W2) + (self.W2 == 0).astype(int) ) + 2 * self.l2 * self.W2
+        self.grad_b2 = self.grad_h2 + self.l1 * self.b2 / ( np.abs(self.b2) + (self.b2 == 0).astype(int) ) + 2 * self.l2 * self.b2
 
         self.grad_h1 = np.dot(self.W2.T, self.grad_h2)
         self.grad_a1 = self.grad_h1 * self.activation.derivative(self.a1)
-        self.grad_w1 = np.dot(self.grad_a1, self.X)
-        self.grad_b1 = self.grad_h1
+        self.grad_w1 = np.dot(self.grad_a1, self.X) + self.l1 * self.W1 / ( np.abs(self.W1) + (self.W1 == 0).astype(int) ) + 2 * self.l2 * self.W1
+        self.grad_b1 = self.grad_h1 + self.l1 * self.b1 / ( np.abs(self.b1) + (self.b1 == 0).astype(int) ) + 2 * self.l2 * self.b1
 
         self.W3 -= learning_rate * self.grad_w3 / batch_size
         self.b3 -= learning_rate * self.grad_b3.sum(axis=1).reshape((-1,1)) / batch_size
